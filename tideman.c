783966ca51b54400d472192c9ea23674285b9aee
Verano-20 <samdawson97@hotmail.co.uk>
2020-05-25 15:21:39 +0100

    My solution

--- tideman.c
+++ tideman.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max number of candidates
 #define MAX 9
@@ -22,8 +23,8 @@ pair;
 string candidates[MAX];
 pair pairs[MAX * (MAX - 1) / 2];
 
-int pair_count;
-int candidate_count;
+int pair_count, candidate_count, i, j, k;
+bool winner;
 
 // Function prototypes
 bool vote(int rank, string name, int ranks[]);
@@ -32,6 +33,9 @@ void add_pairs(void);
 void sort_pairs(void);
 void lock_pairs(void);
 void print_winner(void);
+void mergesort(pair pairs[pair_count], int p, int r);
+void merge(pair pairs[pair_count], int p, int q, int r);
+bool cause_cycle(int subject, int target);
 
 int main(int argc, string argv[])
 {
@@ -49,15 +53,15 @@ int main(int argc, string argv[])
         printf("Maximum number of candidates is %i\n", MAX);
         return 2;
     }
-    for (int i = 0; i < candidate_count; i++)
+    for (i = 0; i < candidate_count; i++)
     {
         candidates[i] = argv[i + 1];
     }
 
     // Clear graph of locked in pairs
-    for (int i = 0; i < candidate_count; i++)
+    for (i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        for (j = 0; j < candidate_count; j++)
         {
             locked[i][j] = false;
         }
@@ -67,13 +71,13 @@ int main(int argc, string argv[])
     int voter_count = get_int("Number of voters: ");
 
     // Query for votes
-    for (int i = 0; i < voter_count; i++)
+    for (int v = 0; v < voter_count; v++)
     {
         // ranks[i] is voter's ith preference
         int ranks[candidate_count];
 
         // Query for each rank
-        for (int j = 0; j < candidate_count; j++)
+        for (j = 0; j < candidate_count; j++)
         {
             string name = get_string("Rank %i: ", j + 1);
 
@@ -83,9 +87,7 @@ int main(int argc, string argv[])
                 return 3;
             }
         }
-
         record_preferences(ranks);
-
         printf("\n");
     }
 
@@ -93,48 +95,231 @@ int main(int argc, string argv[])
     sort_pairs();
     lock_pairs();
     print_winner();
+
     return 0;
+
 }
 
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    // TODO
+    //check candidate name is valid and update ranks array
+    for (i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, candidates[i]) == 0)
+        {
+            ranks[rank] = i;
+            return true;
+        }
+    }
     return false;
 }
 
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    // TODO
+    for (j = 0; j < candidate_count; j++)
+    {
+        for (k = j + 1; k < candidate_count; k++)
+        {
+            preferences[ranks[j]][ranks[k]]++;
+        }
+    }
     return;
 }
 
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
-    return;
+    bool exists;
+
+    //each column in preferences
+    for (i = 0; i < candidate_count; i++)
+    {
+        //each row in preferences
+        for (j = 0; j < candidate_count; j++)
+        {
+            //reset check for if a pair exists
+            exists = false;
+
+            //if more prefer i to j than j to i, i is winner and j is loser
+            if (preferences[i][j] > preferences[j][i])
+            {
+                //check pair doesn't already exist
+                for (k = 0; k < pair_count; k++)
+                {
+                    if (pairs[k].winner == i && pairs[k].loser == j)
+                    {
+                        exists = true;
+                    }
+                }
+                if (exists == false)
+                {
+                    pairs[pair_count].winner = i;
+                    pairs[pair_count].loser = j;
+                    pair_count++;
+                }
+            }
+            //if more prefer j to i than i to j, j is winner and i is loser
+            else if (preferences[j][i] > preferences[i][j])
+            {
+                //check pair doesn't already exist
+                for (k = 0; k < pair_count; k++)
+                {
+                    if (pairs[k].winner == j && pairs[k].loser == i)
+                    {
+                        exists = true;
+                    }
+                }
+                if (exists == false)
+                {
+                    pairs[pair_count].winner = j;
+                    pairs[pair_count].loser = i;
+                    pair_count++;
+                }
+            }
+        }
+    }
 }
 
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    // TODO
-    return;
+    //merge sort the pairs array from 0 to last element, referring to preferences[] for strength of victory
+    mergesort(pairs, 0, pair_count - 1);
+}
+
+
+// function to determine if adding a line to the locked[] graph will cause a cycle
+// subject is current candidate, target is the losing candidate in the considered pair
+bool cause_cycle(int subject, int target)
+{
+    // if the subject is the target, it will lead back to the subject and cause a cycle
+    if (subject == target)
+    {
+        return true;
+    }
+    else
+    {
+        for (int n = 0; n < candidate_count; n++)
+        {
+            if (locked[n][subject] == true)
+            {
+                // recursion to check each layer against the same target
+                if (cause_cycle(n, target))
+                {
+                    return true;
+                }
+            }
+        }
+        // if target does not lead back to subject at any stage
+        return false;
+    }
 }
 
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    // TODO
-    return;
+    for (i = 0; i < pair_count; i++)
+    {
+        if (!cause_cycle(pairs[i].winner, pairs[i].loser))
+        {
+            locked[pairs[i].winner][pairs[i].loser] = true;
+        }
+    }
 }
 
 // Print the winner of the election
 void print_winner(void)
 {
-    // TODO
-    return;
+    // check all candidates in locked[]
+    for (i = 0; i < candidate_count; i++)
+    {
+        //reset winner when checking each candidate
+        winner = true;
+
+        for (j = 0; j < candidate_count; j++)
+        {
+            if (locked[j][i] == true)
+            {
+                winner = false;
+            }
+        }
+        if (winner == true)
+        {
+            printf("%s\n", candidates[i]);
+        }
+    }
 }
 
+
+//input array[], p is first element (0), r is last element (length of array[])
+void mergesort(pair pairs1[pair_count], int p, int r)
+{
+    if (p < r)
+    {
+        int q = (p + r) / 2;
+        mergesort(pairs1, p, q); // sort left half of pairs1
+        mergesort(pairs1, q + 1, r); //sort right half of pairs1
+        merge(pairs1, p, q, r); //merge left (p to q) and right (q+1 to r) halves
+    }
+}
+
+//p is first element of left subarray, q is last element of left subarray, r is last element of right subarray
+void merge(pair pairs1[pair_count], int p, int q, int r)
+{
+    //get length for left array L
+    int n1 = q - p + 1;
+    //get length for right array R
+    int n2 = r - q;
+
+    //declare arrays for left and right halves
+    pair L[n1], R[n2];
+
+    //fill left array
+    for (i = 0; i < n1; i++)
+    {
+        L[i] = pairs1[p + i];
+    }
+
+    //fill right array
+    for (j = 0; j < n2; j++)
+    {
+        R[j] = pairs1[q + j + 1];
+    }
+
+    //track index of sub arrays and main array
+    i = 0;
+    j = 0;
+    k = p;
+
+    //until we reach the end of the left or right array
+    while (i < n1 && j < n2 && k < r + 1)
+    {
+        //pick the larger strength of victory between L and R and put in array
+        if (preferences[L[i].winner][L[i].loser] >= preferences[R[j].winner][R[j].loser])
+        {
+            pairs1[k] = L[i];
+            i++;
+        }
+        else
+        {
+            pairs1[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    //once we run out of elements in either the left of right array, copy the remaining elements into array[k]
+    while (i < n1)
+    {
+        pairs1[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        pairs1[k] = R[j];
+        j++;
+        k++;
+    }
+}
